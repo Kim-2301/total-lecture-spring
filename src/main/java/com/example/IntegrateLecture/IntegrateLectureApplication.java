36d17c47307aecff35fa608f8dfad2cae404e1e9
Kim-2301 <seunghhhh2344@gmail.com>
2024-11-11 01:15:31 +0900

    init

---
+++ src/main/java/com/example/IntegrateLecture/IntegrateLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.IntegrateLecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrateLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IntegrateLecture/IntegrateLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntegrateLecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrateLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
